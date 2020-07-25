d7b54ebc4f0a158a79caf499d128902161620c04
Leon Marbukh <leonmarbukh@gmail.com>
2020-07-25 15:23:37 -0700

    updated some things

--- src/SavedArticles/articles-router.js
+++ src/SavedArticles/articles-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ArticlesService = require("./articles-service");
 const articlesRouter = express.Router();
 const bodyParser = express.json();
+const { requireAuth } = require("../middleware/jwt-auth");
 
 articlesRouter
   .route("/")
@@ -13,7 +14,7 @@ articlesRouter
       })
       .catch(next);
   })
-  .post(bodyParser, (req, res, next) => {
+  .post(requireAuth, bodyParser, (req, res, next) => {
     const { title, url, image, user_id } = req.body;
     const newArticle = { title, url, image, user_id };
     for (const [key, value] of Object.entries(newArticle)) {
@@ -27,18 +28,43 @@ articlesRouter
       .then((article) => {
         res
           .status(201)
-          .location(path.posix.join(req.originalUrl, `/${article.id}`))
+          // .location(path.posix.join(req.originalUrl, `/${article.id}`))
           .json(article);
       })
       .catch(next);
   })
   .delete((req, res, next) => {
-      ArticlesService.deleteArticle(req.app.get("db"), req.body.id)
+    console.log(req.body);
+    ArticlesService.deleteArticle(req.app.get("db"), req.body.id)
       .then((affected) => {
-          res.status(204).end();
+        res.status(204).end();
       })
       .catch(next);
   });
-  
+  // articlesRouter
+  //   .route("/:id")
+  //   .all((req, res, next) => {
+  //     ArticlesService.getById(req.app.get("db"), req.params.id)
+  //       .then((article) => {
+  //         if (!article) {
+  //           return res.status(404).json({
+  //             error: { message: `article doesn't exist` },
+  //           });
+  //         }
+  //         res.id = article;
+  //         next();
+  //       })
+  //       .catch(next);
+  //   })
+  //   .get((req, res, next) => {
+  //     res.json(res.id);
+  //   })
+  //   .delete((req, res, next) => {
+  //     ArticlesService.deleteArticle(req.app.get("db"), req.params.id)
+  //       .then((affected) => {
+  //         res.status(204).end();
+  //       })
+  //       .catch(next);
+  //   });
 
 module.exports = articlesRouter;
